547ecf9fd09af113bcb7ab598a10d2ec9ee10ff6
Chaostheorie <chaos4747@gmail.com>
2019-12-19 17:12:46 +0100

    Add basicauth support

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_basicauth import BasicAuth
 from json.decoder import JSONDecodeError
-from config import config
+from config import config, BasicAuthConfig
 
 
 app = Flask(__name__)
 app.config.from_object(config)
 db = SQLAlchemy(app)
+app.use_auth = False
 app.tournament_name = ""
 app.master_name = ""
 app.running = False
@@ -22,5 +24,8 @@ if app.config["LOAD_PREENV"] is True:
     except JSONDecodeError:
         print("Skipped Preenv loading")
 
+if app.use_auth:
+    app.config.from_object(BasicAuthConfig)
+    auth = BasicAuth(app)
 from app.routes import *
 from app.filters import *
--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+import json
+from json.decoder import JSONDecodeError
 
 # Load data from .env file if avaible
 from dotenv import load_dotenv
@@ -6,6 +8,27 @@ load_dotenv()
 
 # Config for basedir
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+def check_preenv(key):
+    with open("preenv.json", "r+") as f:
+        try:
+            preenv = json.load(f)
+        except JSONDecodeError:
+            print("Preenv skipped")
+        if isinstance(key, list):
+            if len([item for item in key if key not in preenv.keys()]) > 0:
+                return False
+        else:
+            if key not in preenv.keys():
+                return False
+        return True
+
+
+def load_preenv(key):
+    with open("preenv.json") as f:
+        preenv = json.load(f)
+    return preenv[key]
 
 
 class config(object):
@@ -22,3 +45,12 @@ class config(object):
 
     # Basedir
     BASEDIR = basedir
+
+
+class BasicAuthConfig(config):
+    BASIC_AUTH_PASSWORD = (check_preenv("password") and load_preenv("password")) \
+                         or os.urandom(36)
+    BASIC_AUTH_USERNAME = (check_preenv("username") and load_preenv("username")) \
+                         or os.urandom(36)
+    BASIC_AUTH_FORCE = (check_preenv("force") and load_preenv("force")) \
+                      or True
